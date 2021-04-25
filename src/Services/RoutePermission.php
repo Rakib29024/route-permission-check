9341e84ae1e2478b4f27222b6c969d38f1422258
Md. Rakib Hassan <rakib29024@gmail.com>
2021-04-25 17:10:59 +0600

    Modifed for index route

--- src/Services/RoutePermission.php
+++ src/Services/RoutePermission.php
@@ -8,8 +8,12 @@ class RoutePermission
     }
     public function CanView($routes){
         for ($i=0; $i < count($routes); $i++) {
-            $key=explode('.',$routes[$i]);
-            $key=end($key);
+            $list=explode('.',$routes[$i]);
+            $key=end($list);
+            if($key=='index'){
+                $previous_key=$list[count($list)-2];
+                $key=$previous_key.$key;
+            }
             if(canViewRoute($routes[$i],'route')){
                 $data[$key]=$routes[$i];
             }else{
